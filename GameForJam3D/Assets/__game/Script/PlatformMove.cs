65b34325110d65b37b405ca512bdf6983f486953
Shurfer <84514709+Shurfer@users.noreply.github.com>
2021-11-06 18:47:59 +0300

    готовый мост
    мост готов

--- GameForJam3D/Assets/__game/Script/PlatformMove.cs
+++ GameForJam3D/Assets/__game/Script/PlatformMove.cs
@@ -6,22 +6,34 @@ using UnityEngine;
 
 public class PlatformMove : MonoBehaviour
 {
-    public float speedSec;
+   // public float speedSec;
 
-    public Vector3 newPos;
+   // public Vector3 newPos;
     
     public bool stop;
-    public Transform trans;
-  /*  private void OnTriggerEnter(Collider other)
+ //   public Transform trans;
+    public Rigidbody rig;
+   private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "PlayerHealth")
         {
-            trans = other.transform.parent.GetComponent<Transform>();
-            trans.SetParent(transform);
+           // trans = other.transform.parent.GetComponent<Transform>();
+           // trans.SetParent(transform);
+           if(!stop)
+           {
+              // rig.useGravity = true;
+              MovePlatform();
+               Destroy(gameObject, 5);
+           }
         }
-    }*/
+    }
+   
+   void MovePlatform()
+   {
+       transform.DOMove(transform.position-new Vector3(0,10,0), 2).SetEase(Ease.Linear);
+   }
     
-    public Transform platformMove;
+  /*  public Transform platformMove;
     public Transform endPos;
     private void Start()
     {
@@ -33,5 +45,5 @@ public class PlatformMove : MonoBehaviour
     {
         platformMove.position = transform.position;
         platformMove.DOMove(endPos.position, speedSec).SetEase(Ease.Linear).OnComplete(MovePlatform); 
-    }
+    }*/
 }
